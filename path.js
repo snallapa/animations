e7040e24896e9ba0305b2ba11d2861d558869192
Sahith Nallapareddy <sahith.reddy@gmail.com>
2019-04-12 22:12:31 +0800

    mbdtf animation

--- path.js
+++ path.js
@@ -1,10 +1,14 @@
 paths = $$("path");
+let output = "";
 for (let i = 0; i < paths.length; i++) {
   path = paths[i];
-  console.log(`.path${i + 1} {
+  output =
+    output +
+    `path:nth-of-type(${i + 1}) {
     stroke-dasharray: ${path.getTotalLength()};
     stroke-dashoffset: ${path.getTotalLength()};
     animation: dash 5s linear forwards;
     fill-opacity: 0
-}`);
+}\n`;
 }
+console.log(output);
